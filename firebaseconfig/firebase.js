48a7f35cd00e5f2c474e1d6ce8be0738ccb54fa6
unknown <onosogapelumi@gmail.com>
2021-10-29 14:22:05 +0100

    updates

--- firebaseconfig/firebase.js
+++ firebaseconfig/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyBgCX5GjXM7VmYZxbYrsqqOQ8nslPmgFCE",
-  authDomain: "yt-clone-1496b.firebaseapp.com",
-  projectId: "yt-clone-1496b",
-  storageBucket: "yt-clone-1496b.appspot.com",
-  messagingSenderId: "732346878714",
-  appId: "1:732346878714:web:a37f1fdaa555993a951743",
+ //your firebase credentials
 };
 
 const app = initializeApp(firebaseConfig);
